52b01a97cf18accf46941269e5bfb9681cb5adda
Ahmet Fatih Çenesiz <fcenesiz@gmail.com>
2023-01-10 10:42:50 +0300

    room database created

--- app/src/main/java/com/fcenesiz/roomwithmultipletables/SchoolDao.kt
+++ app/src/main/java/com/fcenesiz/roomwithmultipletables/SchoolDao.kt
@@ -8,8 +8,8 @@ import androidx.room.Transaction
 import com.fcenesiz.roomwithmultipletables.entities.Director
 import com.fcenesiz.roomwithmultipletables.entities.School
 import com.fcenesiz.roomwithmultipletables.entities.Student
-import com.fcenesiz.roomwithmultipletables.entities.relations.SchoolAndDirector
-import com.fcenesiz.roomwithmultipletables.entities.relations.SchoolWithStudents
+import com.fcenesiz.roomwithmultipletables.entities.Subject
+import com.fcenesiz.roomwithmultipletables.entities.relations.*
 
 @Dao
 interface SchoolDao {
@@ -23,6 +23,12 @@ interface SchoolDao {
     @Insert(onConflict = REPLACE)
     suspend fun insertStudent(student: Student)
 
+    @Insert(onConflict = REPLACE)
+    suspend fun insertSubject(subject: Subject)
+
+    @Insert(onConflict = REPLACE)
+    suspend fun insertStudentSubjectCrossRef(crossRef: StudentSubjectCrossRef)
+
     @Transaction // to prevent multithreading issue
     @Query("SELECT * FROM school WHERE schoolName = :schoolName")
     suspend fun getSchoolAndDirectorWithSchoolName(schoolName: String): List<SchoolAndDirector>
@@ -30,5 +36,14 @@ interface SchoolDao {
     @Transaction
     @Query("SELECT * FROM school WHERE schoolName = :schoolName")
     suspend fun getSchoolWithStudents(schoolName: String): List<SchoolWithStudents>
+
+    @Transaction
+    @Query("SELECT * FROM subject WHERE subjectName = :subjectName")
+    suspend fun getStudentsOfSubject(subjectName: String): List<SubjectWithStudents>
+
+    @Transaction
+    @Query("SELECT * FROM student WHERE studentName = :studentName")
+    suspend fun getSubjectsOfStudent(studentName: String): List<StudentWithSubjects>
+
 
 }
